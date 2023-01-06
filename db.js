ee6e35a442369fe073c70bb984cbafb9d95b40bc
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-06 12:22:06 -0800

    add .save() method to reservations

--- db.js
+++ db.js
@@ -4,9 +4,15 @@
 
 const { Client } = require("pg");
 
+// DB_URL definition for chuck
+// const DB_URI = process.env.NODE_ENV === "test"
+//     ? "postgresql:///lunchly_test"
+//     : "postgresql:///lunchly";
+
+// DB_URI definition for nico
 const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql:///lunchly_test"
-    : "postgresql:///lunchly";
+  ? "postgresql://nicom:nicom@localhost/lunchly_test"
+  : "postgresql://nicom:nicom@localhost/lunchly"
 
 let db = new Client({
   connectionString: DB_URI,
